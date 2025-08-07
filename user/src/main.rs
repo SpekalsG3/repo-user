c9c974dd21ba826a8c6f3b37d99a7deb79e2d5b2
SpekalsG3 <karpick.kirill@gmail.com>
2025-08-08 01:13:09 +0300

    feat(sub): add usage

--- user/src/main.rs
+++ user/src/main.rs
@@ -1,4 +1,7 @@
 fn main() {
     let res = source::add(5_u64, 10_u64);
-    println!("res: {}", res);
+    println!("res add: {}", res);
+
+    let res = source::sub(5_u64, 2_u64);
+    println!("res sub: {}", res);
 }
